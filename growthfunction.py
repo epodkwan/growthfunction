448c22ebc8a2e947c467fbb1b2d4e07899cdae0b
epodkwan <epod.kwan@gmail.com>
2022-07-06 14:12:07 -0400

    Create growthfunction.py

---
+++ growthfunction.py
@@ -0,0 +1,67 @@
+# %%
+import math
+import matplotlib.pyplot as plt
+
+# %%
+h0=70
+omm=0.3
+omde=0.7
+p=[-3,0,-2]
+ai=0.001
+omlam=1-omm-omde
+om=[omm,omde,omlam]
+par=[om,p]
+
+# %%
+def calh(a):
+    total=0
+    for i in range(3):
+        total=total+par[0][i]*a**par[1][i]
+    h=h0*math.sqrt(total)
+    return h
+
+# %%
+def func(a):
+    temp=1/a/calh(a)
+    return temp*temp*temp
+
+# %%
+def rk4(x0,y0,dx):
+    k=[0,0,0,0]
+    k[0]=func(x0)
+    k[1]=func(x0+dx/2)
+    k[3]=func(x0+dx)
+    return y0+(k[0]+4*k[1]+k[3])*dx/6
+
+# %%
+def integration(x0,xf,dx):
+    x=x0
+    y=0
+    while x<xf:
+        y=rk4(x,y,dx)
+        x=x+dx
+    return y
+
+# %%
+def growth(a):
+    temp=integration(ai,a,0.001)
+    return 5*omm/2*h0*h0*calh(a)*temp
+
+# %%
+def plotgraph():
+    a=0.01
+    temp=growth(1)
+    while a<=1:
+        plt.scatter(a,growth(a)/temp)
+        a=a+0.01
+    plt.ticklabel_format(useOffset=False)
+    plt.show()
+
+# %%
+print("Growth factor=")
+plotgraph()
+
+# %%
+
+
+

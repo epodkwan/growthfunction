0e99c2006cd3a2c4ad079bbef36660c6f771f8e3
epodkwan <epod.kwan@gmail.com>
2022-07-09 14:46:56 -0400

    update

--- error.py
+++ error.py
@@ -5,12 +5,12 @@ from nbodykit.cosmology import background
 
 # %%
 h0=70
-omm=0.3
-omde=0.7
+om_m=0.3
+om_de=0.7
 p=[-3,0,-2]
 a_initial=0.001
-omlam=1-omm-omde
-om=[omm,omde,omlam]
+om_lam=1-om_m-om_de
+om=[om_m,om_de,om_lam]
 par=[om,p]
 
 # %%
@@ -46,7 +46,7 @@ def integration(x_initial,x_final,dx):
 # %%
 def growth(a):
     temp=integration(a_initial,a,0.001)
-    return 5*omm/2*h0*h0*calh(a)*temp
+    return 5*om_m/2*h0*h0*calh(a)*temp
 
 # %%
 def plotgraph():
@@ -60,10 +60,10 @@ def plotgraph():
 
 # %%
 def error():
-    test=background.MatterDominated(om_m,om_lam,om_k,a=None,a_normalize=1.0)
+    test=background.MatterDominated(om_m,om_lam,om_de,a=None,a_normalize=1.0)
     a=0.01
     while a<=1:
-        plt.scatter(a,calh(a)/test.D1(a)-1)
+        plt.scatter(a,growth(a)/test.D1(a)-1)
         a=a+0.01
     plt.show()
 

d7c86ec0cd91fa27aee7b3ea18a79464f62f6347
epodkwan <epod.kwan@gmail.com>
2022-07-13 12:35:18 -0400

    emulator

--- emulator.py
+++ emulator.py
@@ -7,30 +7,13 @@ model=torch.load('model.pth')
 model.eval()
 
 # %%
-data=torch.ones(2000,1)
-data[:,0]=a
-data[:,1]=b
-data[:,2]=c
-data[:,3]=x_data
+parameters=torch.ones(1,1)
+parameters[:,0]=1
+# parameters[:,1]=2
+# parameters[:,2]=3
+# parameters[:,3]=4
+# parameters[:,4]=5
 
 # %%
-test=torch.ones(600,1)
-test_x=torch.linspace(-3,3,600)
-test[:,0]=test_x
-true_y=func(test_x)
-test_y=model(test).clone().detach()
-print(test_y)
-error=abs(test_y/true_y-1)
-print("Max error =",torch.max(error).item()*100,"%")
-plt.subplot(211)
-plt.plot(test_x,true_y,label='Reference')
-plt.plot(test_x,test_y,label='Fitting')
-plt.ylabel("y")
-plt.title("Comparison")
-plt.legend()
-plt.subplot(212)
-plt.plot(test_x,error,label='Error')
-plt.xlabel("x")
-plt.ylabel("Error (*100%)")
-plt.title("Error")
-plt.show()
+d=model(parameters).clone().detach()
+print(d)

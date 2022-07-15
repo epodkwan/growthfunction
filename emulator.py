d40902c1d9211b59ea496f9bc16033fa3af22314
epodkwan <epod.kwan@gmail.com>
2022-07-15 17:43:29 -0400

    testing

--- emulator.py
+++ emulator.py
@@ -1,5 +1,6 @@
 # %%
 import math
+import random
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,7 +15,7 @@ model.eval()
 
 # %%
 cosmo=npy_loader("data/cosmo.npy")
-cosmo_num=randrange(1000)
+cosmo_num=random.randrange(1000)
 parameters=torch.ones(1,2)
 parameters[:,0]=cosmo[cosmo_num,0]
 parameters[:,1]=cosmo[cosmo_num,2]
@@ -22,22 +23,16 @@ a=torch.flatten(npy_loader("data/"+str(cosmo_num)+".npy").narrow(0,0,1),0,-1)
 a=1/(a+1)
 d_data=torch.flatten(npy_loader("data/"+str(cosmo_num)+".npy").narrow(0,1,1),0,-1)
 d_test=torch.flatten(model(parameters).clone().detach(),0,-1)
+discrepancy=d_test/d_data
+plt.subplot(211)
 plt.plot(a,d_data,label="Reference")
 plt.plot(a,d_test,label="Fitting")
-plt.xlabel("a")
 plt.ylabel("D")
 plt.title("Comparison")
 plt.legend()
-plt.safefig("cosmo"+str(cosmo_num)+".png")
-
-# plt.subplot(211)
-# plt.plot(a,d_data,label="Reference")
-# plt.plot(a,d_test,label="Fitting")
-# plt.ylabel("D")
-# plt.title("Comparison")
-# plt.legend()
-# plt.subplot(212)
-# plt.plot(test_x,error)
-# plt.xlabel("a")
-# plt.ylabel("Predict/Data")
-# plt.title("Discrepancy")
+plt.subplot(212)
+plt.plot(a,discrepancy)
+plt.xlabel("a")
+plt.ylabel("Predict/Data")
+plt.title("Discrepancy")
+plt.savefig("cosmo"+str(cosmo_num)+".png")

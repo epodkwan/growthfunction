e2987b5b5784ebb14809e89ad4de340a1559f2b9
epodkwan <epod.kwan@gmail.com>
2022-07-15 07:58:13 -0400

    testing

--- trainer.py
+++ trainer.py
@@ -1,76 +1,31 @@
 # %%
 import math
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 torch.set_num_threads(8)
 
 # %%
-h0=70
-om_m=0.3
-om_lam=0.7
-p=[-3,0,-2]
-a_initial=0.00001
-om_k=1-om_m-om_lam
-om=[om_m,om_lam,om_k]
-par=[om,p]
+def npy_loader(path):
+    return torch.from_numpy(np.load(path))
 
 # %%
-def calh(a,par):
-    total=0
-    for i in range(3):
-        total=total+par[0][i]*a**par[1][i]
-    h=h0*math.sqrt(total)
-    return h
-
-# %%
-def func(a,par):
-    temp=1/a/calh(a,par)
-    return temp*temp*temp
-
-# %%
-def rk4(x0,y0,dx,par):
-    k=[0,0,0,0]
-    k[0]=func(x0,par)
-    k[1]=func(x0+dx/2,par)
-    k[3]=func(x0+dx,par)
-    return y0+(k[0]+4*k[1]+k[3])*dx/6
-
-# %%
-def integration(x_initial,x_final,dx,par):
-    x=x_initial
-    y=0
-    while x<x_final:
-        y=rk4(x,y,dx,par)
-        x=x+dx
-    return y
-
-# %%
-def growth(a,par):
-    temp=integration(a_initial,a,0.00001,par)
-    return 5*par[0][0]/2*h0*h0*calh(a,par)*temp
-
-# %%
-def normalize(a,par1,par0):
-    return growth(a,par1)/growth(1,par0)
-
-# %%
-x_data=torch.linspace(0.01,1,200)
-y_data=torch.ones(200)
-for i in range(200):
-    y_data[i]=normalize(x_data[i],par,par)
-data=torch.ones(200,3)
-data[:,0]=om_m
-data[:,1]=h0
-data[:,2]=x_data
+input_data=npy_loader("data/cosmo.npy")
+x_train=torch.cat((input_data.narrow(1,0,1),input_data.narrow(1,2,1)),1).narrow(0,0,900)
+y_train=torch.ones(900,256)
+for i in range(900):
+    temp=npy_loader("data/"+str(i)+".npy")
+    y_train[i,:]=temp[1,:]
+print(y_train)
 
 # %%
 model=torch.nn.Sequential(
-    torch.nn.Linear(3,16),
+    torch.nn.Linear(2,4),
+    torch.nn.ReLU(),
+    torch.nn.Linear(4,16),
     torch.nn.ReLU(),
     torch.nn.Linear(16,256),
-    torch.nn.ReLU(),
-    torch.nn.Linear(256,1),
-    torch.nn.Flatten(0, 1)
+    torch.nn.Flatten(0,256)
 )
 
 # %%
@@ -81,8 +36,8 @@ optimizer=torch.optim.SGD(model.parameters(),lr=learning_rate,momentum=0.9)
 
 # %%
 for t in range(epochs):
-    y_pred=model(data)
-    loss=loss_fn(y_pred,y_data)
+    y_pred=model(x_train)
+    loss=loss_fn(y_pred,y_train)
     if t % 100 == 99:
         print(t,loss.item())
         plt.scatter(t,torch.log(loss.detach()),c="b")
@@ -97,15 +52,11 @@ plt.title("Loss function")
 plt.show()
 
 # %%
-test=torch.ones(100,3)
-test_x=torch.linspace(0.01,1,100)
-test[:,0]=om_m
-test[:,1]=h0
-test[:,2]=test_x
+x_test=torch.cat((input_data.narrow(1,0,1),input_data.narrow(1,2,1)),1).narrow(0,900,100)
 true_y=torch.ones(100)
 for i in range(100):
     true_y[i]=normalize(test_x[i],par,par)
-test_y=model(test).clone().detach()
+test_y=model(x_test).clone().detach()
 print(test_y)
 error=abs(test_y/true_y-1)
 print("Max error =",torch.max(error).item()*100,"%")

ba1c9a2792f0345ec7b3ef7b198733fe230b4b99
epodkwan <epod.kwan@gmail.com>
2022-07-22 18:04:24 -0400

    testing

--- jaxtrainer.py
+++ jaxtrainer.py
@@ -4,7 +4,7 @@ import jax
 import numpy as np
 import jax.numpy as jnp
 from jax import grad,jit,vmap,random
-from jax.experimental import optimizers as jax_opt
+import jax.example_libraries.optimizers as jax_opt
 import matplotlib.pyplot as plt
 
 # %%
@@ -13,16 +13,16 @@ def npy_loader(path):
 
 # %%
 input_data=npy_loader("data/cosmo.npy")
-x_train=torch.cat((input_data.narrow(1,0,1),input_data.narrow(1,2,1)),1).narrow(0,0,800).float()
-y_train=torch.ones(800,256)
+x_train=jnp.stack((input_data[0:800,0],input_data[0:800,2]),axis=1)
+y_train=jnp.ones((800,256))
 for i in range(800):
     temp=npy_loader("data/"+str(i)+".npy")
-    y_train[i,:]=temp[1,:]
-x_validate=torch.cat((input_data.narrow(1,0,1),input_data.narrow(1,2,1)),1).narrow(0,800,100).float()
-y_validate=torch.ones(100,256)
+    y_train.at[i,:].set(temp[1,:])
+x_validate=jnp.stack((input_data[800:900,0],input_data[800:900,2]),axis=1)
+y_validate=jnp.ones((100,256))
 for i in range(100):
     temp=npy_loader("data/"+str(i+800)+".npy")
-    y_validate[i,:]=temp[1,:]
+    y_validate.at[i,:].set(temp[1,:])
 
 # %%
 def random_layer_params(m,n,key,scale=1e-2):
@@ -53,54 +53,38 @@ def predict(params,data):
 batched_predict=vmap(predict,in_axes=(None,0))
 
 # %%
-def mse_loss(params,loss_data):
-    X_tbatch,targets=loss_data
-    preds=batched_predict(params,X_tbatch)
-    diff=preds-targets 
+@jit
+def mse_loss(params,x,y_ref):
+    preds=batched_predict(params,x)
+    diff=preds-y_ref
     return jnp.mean(diff*diff)
 
 #%%
-opt_init,opt_update,get_params=jax_opt.adam(1e-3)
-opt_state=opt_intial(params)
+opt_init,opt_update,get_params=jax_opt.adam(learning_rate)
+opt_state=opt_init(params)
 
+# %%
 @jit
-def train_step(step_i,opt_state,loss_data):
-    net_params=get_params(opt_state)
-    loss=mse_loss(params,data)
-    loss,grads=value_and_grad(mse_loss,argnums=0)(net_params,loss_data)
+def train_step(step_i,opt_state,x,y_ref):
+    params=get_params(opt_state)
+    loss,grads=value_and_grad(mse_loss,argnums=0)(params,x,y_ref)
     return loss,opt_update(step_i,grads,opt_state)
 
 # %%
-for epoch in range(num_epochs):
-    for x, y in training_generator:
-        params = update(params, x, y)
-
-# %%
-loss_fn=torch.nn.MSELoss(reduction='mean')
-optimizer=torch.optim.SGD(model.parameters(),lr=learning_rate,momentum=0.9)
-
-# %%
 for i in range(epochs):
-    order=torch.randperm(800)
     train_loss=0
     for j in range(25):
         x_batch=x_train[order[32*j:32*(j+1)-1],:]
         y_batch=y_train[order[32*j:32*(j+1)-1],:]
-        y_pred=model(x_batch)
-        loss=loss_fn(y_pred,y_batch)
-        train_loss=train_loss+loss_fn(y_pred,y_batch)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        training_loss,opt_state=train_step(learning_rate,x_batch,opt_state)
     if i % 100 == 99:
         print((i+1),loss.item())
         train_loss=train_loss/25
         y_pred=model(x_validate)
         validate_loss=loss_fn(y_pred,y_validate)
-        plt.scatter((i+1),torch.log(train_loss.detach()),c='b')
-        plt.scatter((i+1),torch.log(validate_loss.detach()),c='g')
+        plt.scatter((i+1),jnp.log(train_loss.detach()),c='b')
+        plt.scatter((i+1),jnp.log(validate_loss.detach()),c='g')
 print("Training ended")
-torch.save(model,"model.pth")
 plt.xlabel("Epoch")
 plt.ylabel("ln(loss)")
 plt.title("Loss function")
@@ -108,12 +92,12 @@ plt.legend(["Training Loss","Validation Loss"])
 plt.savefig("loss.png")
 
 # %%
-x_test=torch.cat((input_data.narrow(1,0,1),input_data.narrow(1,2,1)),1).narrow(0,900,100).float()
-y_test=torch.ones(100,256)
+x_test=jnp.stack((input_data[900:1000,0],input_data[900:1000,2]),axis=1)
+y_test=jnp.ones((100,256))
 for i in range(100):
     temp=npy_loader("data/"+str(i+900)+".npy")
-    y_test[i,:]=temp[1,:]
+    y_test.at[i,:].set(temp[1,:])
 y_pred=model(x_test).clone().detach()
 print(y_pred)
 error=abs(y_pred/y_test-1)
-print("Max error =",torch.max(error).item()*100,"%")
+print("Max error =",jnp.max(error).item()*100,"%")

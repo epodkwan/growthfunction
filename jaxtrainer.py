60dca3bf6410438d20b592d5c2ce4f76b682cbfe
epodkwan <epod.kwan@gmail.com>
2022-07-25 15:10:10 -0400

    testing

--- jaxtrainer.py
+++ jaxtrainer.py
@@ -37,7 +37,7 @@ def init_network_params(sizes,key):
 # %%
 layer_sizes=[2,64,256,256,256]
 learning_rate=1e1
-epochs=30000
+epochs=1000
 params=init_network_params(layer_sizes,random.PRNGKey(0))
 
 # %%
@@ -73,7 +73,7 @@ def train_step(step_i,opt_state,x,y_ref):
 # %%
 order=jnp.arange(800)
 for i in range(epochs):
-    random.permutation(random.PRNGKey(0),order)
+    random.permutation(random.PRNGKey(i),order)
     train_loss=0
     for j in range(25):
         x_batch=x_train[order[32*j:32*(j+1)-1],:]
@@ -83,24 +83,23 @@ for i in range(epochs):
     if i % 100 == 99:
         print((i+1),train_loss)
         train_loss=train_loss/25
-    #     y_pred=predict(params,x_validate)
-    #     validate_loss=loss_fn(y_pred,y_validate)
+        validate_loss=mse_loss(params,x_validate,y_validate)
         plt.scatter((i+1),jnp.log(train_loss),c='b')
-    #     plt.scatter((i+1),jnp.log(validate_loss),c='g')
+        plt.scatter((i+1),jnp.log(validate_loss),c='g')
 print("Training ended")
 plt.xlabel("Epoch")
 plt.ylabel("ln(loss)")
 plt.title("Loss function")
-# plt.legend(["Training Loss","Validation Loss"])
+plt.legend(["Training Loss","Validation Loss"])
 plt.savefig("loss.png")
 
 # %%
-# x_test=jnp.stack((input_data[900:1000,0],input_data[900:1000,2]),axis=1)
-# y_test=jnp.ones((100,256))
-# for i in range(100):
-#     temp=npy_loader("data/"+str(i+900)+".npy")
-#     y_test.at[i,:].set(temp[1,:])
-# y_pred=model(x_test).clone().detach()
-# print(y_pred)
-# error=abs(y_pred/y_test-1)
-# print("Max error =",jnp.max(error)*100,"%")
+x_test=jnp.stack((input_data[900:1000,0],input_data[900:1000,2]),axis=1)
+y_test=jnp.ones((100,256))
+for i in range(100):
+    temp=npy_loader("data/"+str(i+900)+".npy")
+    y_test.at[i,:].set(temp[1,:])
+y_pred=predict(params,x_test)
+print(y_pred)
+error=abs(y_pred/y_test-1)
+print("Max error =",jnp.max(error)*100,"%")

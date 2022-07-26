cdd4b3da269ff9bacf9535b34026bc631f82bf4b
epodkwan <epod.kwan@gmail.com>
2022-07-26 17:47:24 -0400

    testing

--- jaxtrainer.py
+++ jaxtrainer.py
@@ -1,10 +1,11 @@
 # %%
-import math
+from typing import Sequence
 import jax
+import optax
 import numpy as np
 import jax.numpy as jnp
-from jax import grad,jit,vmap,random
-import jax.example_libraries.optimizers as jax_opt
+from jax import jit,random
+from flax import linen as nn
 import matplotlib.pyplot as plt
 
 # %%
@@ -20,50 +21,44 @@ x_validate=jnp.stack((input_data[800:900,0],input_data[800:900,2]),axis=1)
 y_validate=input_result[800:900,:]
 
 # %%
-def random_layer_params(m,n,key,scale=1e-2):
-    w_key,b_key=random.split(key)
-    return scale*random.normal(w_key,(n,m)),scale*random.normal(b_key,(n,))
+class SimpleMLP(nn.Module):
+    features:Sequence[int]
+
+    @nn.compact
+    def __call__(self,inputs):
+        x=inputs
+        for i,feat in enumerate(self.features):
+            x=nn.Dense(feat)(x)
+            if i != len(self.features)-1:
+                x=nn.relu(x)
+        return x
 
 # %%
-def init_network_params(sizes,key):
-    keys=random.split(key,len(sizes))
-    return [random_layer_params(m,n,k) for m,n,k in zip(sizes[:-1],sizes[1:],keys)]
-
-# %%
-layer_sizes=[2,64,256,256,256]
-learning_rate=1e0
-epochs=10000
-params=init_network_params(layer_sizes,random.PRNGKey(0))
-
-# %%
-def predict(params,data):
-    activations=data
-    for w,b in params[:-1]:
-        outputs=jnp.dot(w,activations)+b
-        activations=jax.nn.relu(outputs)
-    final_w,final_b=params[-1]
-    return jnp.dot(final_w,activations)+final_b
-
-# %%
-batched_predict=vmap(predict,in_axes=(None,0))
+layer_sizes=[64,256,256,256]
+learning_rate=1e1
+epochs=1000
+model=SimpleMLP(features=layer_sizes)
+temp=jnp.ones(2)
+params=model.init(random.PRNGKey(0),temp)
 
 # %%
 @jit
 def mse_loss(params,x,y_ref):
-    preds=batched_predict(params,x)
+    preds=model.apply(params,x)
     diff=preds-y_ref
     return jnp.mean(diff*diff)
 
 #%%
-opt_init,opt_update,get_params=jax_opt.adam(learning_rate)
-opt_state=opt_init(params)
+tx=optax.adam(learning_rate=learning_rate)
+opt_state=tx.init(params)
 
 # %%
 @jit
-def train_step(step_i,opt_state,x,y_ref):
-    params=get_params(opt_state)
+def train_step(params,opt_state,x,y_ref):
     loss,grads=jax.value_and_grad(mse_loss,argnums=0)(params,x,y_ref)
-    return loss,opt_update(step_i,grads,opt_state)
+    updates,opt_state=tx.update(grads,opt_state)
+    params=optax.apply_updates(params,updates)
+    return loss,params,opt_state
 
 # %%
 order=jnp.arange(800)
@@ -73,8 +68,7 @@ for i in range(epochs):
     for j in range(25):
         x_batch=x_train[order[32*j:32*(j+1)-1],:]
         y_batch=y_train[order[32*j:32*(j+1)-1],:]
-        loss,opt_state=train_step(learning_rate,opt_state,x_batch,y_batch)
-        params=get_params(opt_state)
+        loss,params,opt_state=train_step(params,opt_state,x_batch,y_batch)
         train_loss=train_loss+loss
     if i % 10 == 9:
         train_loss=train_loss/25
@@ -93,7 +87,7 @@ plt.savefig("loss.png")
 # %%
 x_test=jnp.stack((input_data[900:1000,0],input_data[900:1000,2]),axis=1)
 y_test=input_result[900:1000,:]
-y_pred=batched_predict(params,x_test)
+y_pred=model.apply(params,x_test)
 print(y_pred)
 error=abs(y_pred/y_test-1)
 print("Max error =",jnp.max(error)*100,"%")
